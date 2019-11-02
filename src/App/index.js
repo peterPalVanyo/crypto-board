4a84a12c1aa42b929fc9b656d6f8dc3dfca278fd
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-02 09:30:22 +0100

    AppLayout as a wrapper

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components'
+import Applayout from './AppLayout'
 
 
 function App() {
   return (
-    <div>
+    <Applayout>
       <WelcomeMessage/>
-    </div>
+    </Applayout>
   );
 }
 
