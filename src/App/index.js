c6d533b5846f0ed3ceebd4761e9a8a5e002a4aa7
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-02 09:43:23 +0100

    simple AppBar with grid

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,13 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components'
 import Applayout from './AppLayout'
+import AppBar from './AppBar'
 
 
 function App() {
   return (
     <Applayout>
+      <AppBar/>
       <WelcomeMessage/>
     </Applayout>
   );
