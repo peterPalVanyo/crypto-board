b2b80b3cd0b39a649f719464f33615030d155673
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-02 06:46:18 +0100

    add styled-component to the module

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage'
+import styled, {css} from 'styled-components'
+
 
 function App() {
   return (
-    <div className="App">
-      <p>hello crpto</p>
+    <div>
+      <WelcomeMessage/>
     </div>
   );
 }
